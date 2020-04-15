3f6eb398c92894fcaca38e520346f3af2aa7f02d
xuzhiyong28 <jip0574@qq.com>
2020-04-15 11:22:50 +0800

    提交代码提交代码

--- ant-design-vue-jeecg/src/components/layouts/TabLayout.vue
+++ ant-design-vue-jeecg/src/components/layouts/TabLayout.vue
@@ -162,7 +162,7 @@
 
       // update-begin-author:sunjianlei date:20200120 for: 动态更改页面标题
       changeTitle(title) {
-        let projectTitle = "Jeecg-Boot 企业级快速开发平台"
+        let projectTitle = "福建医佰汇健康产业管理平台"
         // 首页特殊处理
         if (this.$route.path === indexKey) {
           document.title = projectTitle
